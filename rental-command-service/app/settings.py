618cdc5751b8751f2c9e562d382509b0227056c7
Safronov Nicholas <wolfy@Safronovs-MacBook-Air.local>
2025-11-30 00:29:42 +0300

    global Dockerfile and db fix

--- rental-command-service/app/settings.py
+++ rental-command-service/app/settings.py
@@ -4,16 +4,21 @@ from pydantic import AnyHttpUrl, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    STATIONS_SERVICE_URL: AnyHttpUrl
-    PAYMENTS_SERVICE_URL: AnyHttpUrl
-    OFFER_SERVICE_URL: AnyHttpUrl
-    SERVICE_PORT: int = Field(8003, env="SERVER_PORT")
+
+    SERVICE_PORT: int = 8003
+    
+    OFFER_SERVICE_URL: str = "http://localhost:8002"
+    STATIONS_SERVICE_URL: str = "http://localhost:8005"
+    PAYMENTS_SERVICE_URL: str = "http://localhost:8006"
+    SERVICE_PORT: int = Field(8001, env="SERVER_PORT")
+
     OUTBOX_POLL_INTERVAL_SECONDS: int = 5
     HTTP_TIMEOUT_SECONDS: int = 5
     STATIONS_RETRY: int = 3
     PAYMENTS_RETRY: int = 3
-    DB_URL_SHARD_0: Optional[str] = "postgresql+asyncpg://rental-cmd-db-master/rental_cmd_shard_0"
-    DB_URL_SHARD_1: Optional[str] = "postgresql+asyncpg://rental-cmd-db-master/rental_cmd_shard_1"
+    DB_URL_SHARD_0: Optional[str] = "postgresql+asyncpg://postgres:password@rental-cmd-db-master:5432/rental_cmd"
+    DB_URL_SHARD_1: Optional[str] = "postgresql+asyncpg://postgres:password@rental-cmd-db-master:5432/rental_cmd"
+
 
     model_config = {"env_file": ".env", "extra": "ignore"}
 
